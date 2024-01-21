3ee6c21ce2041ac9663b1f3692e3a6069ac74ca2
piyushuprety <84117283+piyushuprety@users.noreply.github.com>
2024-01-21 18:10:02 +0530

    Add files via upload
    initial commit

---
+++ app.js
@@ -0,0 +1,90 @@
+// const { Octokit } = require('@octokit/rest');
+// const octokit = new Octokit();
+const  axios =  require("axios")
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+const PORT = 8000;
+
+const corsOpts = {
+  origin: '*',
+  credentials: true,
+  methods: ['GET', 'POST', 'HEAD', 'PUT', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type'],
+  exposedHeaders: ['Content-Type'],
+};
+app.use(cors(corsOpts));
+
+async function fetchTotalRepo(username) {
+  try {
+    const response = await axios.get(
+     ' https://api.github.com/users/freeCodeCamp'
+    );
+    const totalRepos = response.data.public_repos;
+
+    return totalRepos;
+  } catch (error) {
+    console.error('Error fetching total repositories:', error);
+    throw error;
+  }
+}
+
+async function fetchAllRepositories(limit ,page) {
+  const data = {
+    repositories: [],
+  };
+
+  try {
+    // const { data: repositories } = await octokit.repos.listForUser({
+    //   username,
+    //   per_page:limit,
+    //   page:page ,
+    // });
+
+    const { data: repositories } = await axios.get(
+      `https://api.github.com/users/freeCodeCamp/repos?per_page=${limit}&page=${page}`
+    );
+
+    data.repositories = repositories.map((repo,id) => ({
+      name: repo.name || '',
+      description: repo.description || '',
+      topics: repo.topics || [],
+      seq:id
+    })); 
+
+    return data;
+  } catch (error) {
+    console.error('Error fetching repositories:', error);
+    throw error;
+  }
+}
+
+
+app.listen(PORT, () => {
+  console.log(`listening on PORT: ${PORT}`);
+});
+
+ app.get('/data', async (req, res) => {
+  let page = req.query.page;
+  let limit = req.query.limit;
+  console.log(page,limit)
+  try {
+    const number = await fetchTotalRepo();
+
+    const repoNames = await fetchAllRepositories(limit, page);
+
+    const data = repoNames;
+    console.log(200);
+    res.json(data);
+  } catch (error) {
+    console.error('Failed to fetch repositories:', error);
+    res
+      .status(500)
+      .json({ success: false, error: 'Failed to fetch repositories' });
+  }
+});
+
+
+
